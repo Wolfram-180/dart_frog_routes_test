4c65ed53eb10f7e987f6880b919852bb81868449
Wolfram-180 <sergei.ulvis@gmail.com>
2023-03-04 18:10:16 +0300

    upd

--- routes/index.dart
+++ routes/index.dart
@@ -1,11 +1,5 @@
 import 'package:dart_frog/dart_frog.dart';
 
-Future<Response> onRequest(RequestContext context) async {
-  // Access the incoming request.
-  final request = context.request;
-
-  // Access the request body as parsed `JSON`.
-  final body = await request.json();
-
-  return Response.json(body: {'request_body': body});
+Response onRequest(RequestContext context) {
+  return Response(body: 'Welcome to Dart Frog!');
 }
