0769df042c4edcc557decc7f6521501fb0b656a9
Wolfram-180 <sergei.ulvis@gmail.com>
2023-03-04 18:59:21 +0300

    user model added

--- routes/index.dart
+++ routes/index.dart
@@ -1,11 +1,8 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:dart_frog_mysql/models/user.dart';
 
-Future<Response> onRequest(RequestContext context) async {
-  // Access the incoming request.
-  final request = context.request;
-
-  // Access the request body form data.
-  final body = await request.formData();
-
-  return Response.json(body: {'request_body': body});
+Response onRequest(RequestContext context) {
+  return Response.json(
+    body: User(name: 'Dash', age: 42),
+  );
 }
