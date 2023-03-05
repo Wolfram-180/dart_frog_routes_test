882e734a83f2af53b59a0c352f8643140a6a8587
Wolfram-180 <sergei.ulvis@gmail.com>
2023-03-05 19:28:09 +0300

    upd tests

--- routes/index.dart
+++ routes/index.dart
@@ -1,8 +1,16 @@
 import 'package:dart_frog/dart_frog.dart';
-import 'package:dart_frog_mysql/models/user.dart';
+// import 'package:dart_frog_mysql/models/user.dart';
 
+Response onRequest(RequestContext context) {
+  return Response(
+    body: 'Hello World',
+  );
+}
+
+/*
 Response onRequest(RequestContext context) {
   return Response.json(
     body: User(name: 'Dash', age: 42),
   );
 }
+*/
